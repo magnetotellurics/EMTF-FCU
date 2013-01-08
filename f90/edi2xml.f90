b98238c025d74eb05463134436ceda356fc59d76
Anna Kelbert <anna.kelbert@cfa.harvard.edu>
2013-01-08 01:42:32 +0000

    Commiting the framework for the edi2xml converter. EDI reading not implemented yet.

---
+++ f90/edi2xml.f90
@@ -0,0 +1,250 @@
+program edi2xml
+
+  use global
+  use config
+  use edi_read
+  use xml_write
+  implicit none
+
+  character(len=80) :: input_dir='./'
+  character(len=80) :: z_file=''
+  character(len=80) :: xml_file=''
+  character(len=80) :: config_file = 'config.xml'
+  character(len=80) :: zsitename, basename, verbose=''
+  type(RemoteRef_t) :: Info
+  type(UserInfo_t)  :: UserInfo
+  type(Site_t)      :: zLocalSite, xmlLocalSite, xmlRemoteSite
+  type(Run_t), dimension(:), pointer     :: Run, RemoteRun
+  type(FreqInfo_t), dimension(:), allocatable :: F
+  type(Channel_t), dimension(:), pointer	  :: InputChannel
+  type(Channel_t), dimension(:), pointer	  :: OutputChannel
+  character(100), dimension(:), pointer       :: Notes
+  complex(8), dimension(:,:,:), allocatable    :: TF
+  real(8),    dimension(:,:,:), allocatable    :: TFVar
+  character(10), dimension(:,:), allocatable   :: TFName
+  complex(8), dimension(:,:,:), allocatable    :: InvSigCov
+  complex(8), dimension(:,:,:), allocatable    :: ResidCov
+  real(8),    dimension(:,:), allocatable      :: U,V ! rotation matrices
+  logical           :: config_exists, site_list_exists
+  logical			:: run_list_exists, channel_list_exists
+  integer           :: i, j, k, n, l, istat
+
+  n = command_argument_count()
+
+  if (n<1) then
+     write(0,*) 'Please specify the name of the input Z-file'
+     stop
+  else if (n>=1) then
+     call get_command_argument(1,z_file)
+  end if
+
+  if (n>1) then
+     call get_command_argument(2,xml_file)
+  else
+     l = len_trim(z_file)
+     basename = z_file(1:l-4)
+     xml_file = trim(basename)//'.xml'
+  end if
+
+  if (n>2) then
+     call get_command_argument(3,verbose)
+     if (index(verbose,'silent')>0) then
+        silent = .true.
+     end if
+  end if
+
+  ! Update output file name
+  if (index(xml_file,'.')==0) then
+ 	xml_file = trim(xml_file)//'.xml'
+  end if
+
+  ! Look for configuration file in the input directory
+  i = index(z_file,'/',.true.)
+  if (i>0) then
+    input_dir = z_file(1:i-1)
+  end if
+  config_file = trim(input_dir)//'/'//trim(config_file)
+  if (.not. silent) then
+    write(*,*) 'Reading configuration file ',config_file
+  end if
+
+  ! Read the configuration file, if it exists
+  inquire (file=config_file,exist=config_exists)
+  if (config_exists) then
+  	call read_xml_config(config_file,UserInfo,input_dir)
+  else
+  	write(0,*) 'Please provide an XML configuration file [config.xml].'
+  	write(0,*) 'This file should reside together with the input data.'
+  	write(0,*) 'An example configuration is provided below:'
+  	write(0,*)
+  	write(0,*) '<Configuration>'
+  	write(0,*) '	<Project>USArray</Project>'
+  	write(0,*) '	<Survey>TA</Survey>'
+  	write(0,*) '	<YearCollected>2011</YearCollected>'
+  	write(0,*) '	<Tags>impedance,tipper</Tags>' 
+  	write(0,*) '	<ReleaseStatus>Unrestricted Release</ReleaseStatus>'
+  	write(0,*) '	<AcquiredBy>Zonge Inc.</AcquiredBy>'
+  	write(0,*) '	<Creator>'
+  	write(0,*) '		<Name>Anna Kelbert</Name>'
+  	write(0,*) '		<Email>anya@coas.oregonstate.edu</Email>'
+  	write(0,*) '		<Org>Oregon State University</Org>'
+  	write(0,*) '		<OrgUrl>http://oregonstate.edu</OrgUrl>'
+  	write(0,*) '	</Creator>'
+  	write(0,*) '	<Submitter>'
+  	write(0,*) '		<Name>Anna Kelbert</Name>'
+  	write(0,*) '		<Email>anya@coas.oregonstate.edu</Email>'
+  	write(0,*) '		<Org>Oregon State University</Org>'
+  	write(0,*) '		<OrgUrl>http://oregonstate.edu</OrgUrl>'		
+  	write(0,*) '	</Submitter>'
+  	write(0,*) '	<ProcessedBy>Anna Kelbert</ProcessedBy>'
+  	write(0,*) '	<ProcessingSoftware>'
+  	write(0,*) '		<Name>EMTF 1.0</Name>'
+  	write(0,*) '		<LastMod>1987-10-12</LastMod>'
+  	write(0,*) '		<Author>Gary Egbert</Author>'
+  	write(0,*) '	</ProcessingSoftware>'
+  	write(0,*) '	<OrthogonalGeographic>1</OrthogonalGeographic>'
+  	write(0,*) '	<RunList>Runs.xml</RunList>'
+  	write(0,*) '	<SiteList>Sites.xml</SiteList>'
+  	write(0,*) '	<ChannelList>Channels.xml</ChannelList>'
+  	write(0,*) '<Configuration>'
+  	write(0,*)
+  	write(0,*) 'Project and YearCollected (if present) help identify'
+    write(0,*) 'a product in SPADE. They should not contain spaces.'
+  	write(0,*) 'The optional OrthogonalGeographic field, if true,'
+  	write(0,*) 'will rotate the data to orthogonal geographic coords.'
+  	write(0,*) 'Use with caution: information about the original '
+  	write(0,*) 'measurement coordinates might be lost in the process.'
+    write(0,*) 'Rotation only works if there are four or five channels.'
+  	write(0,*) 'Leave the RunList and SiteList elements out or empty'
+  	write(0,*) 'if you do not have XML lists for this experiment.'
+  	stop
+  end if
+
+  ! Initialize input and output
+  call initialize_xml_output(xml_file,'EM_TF')
+
+  ! Read the Z-file in full
+
+  call initialize_z_input(z_file)
+
+  call read_z_header(zsitename, zLocalSite, Info)
+
+  ! Allocate space for channels and transfer functions
+  allocate(InputChannel(2), OutputChannel(nch-2), stat=istat)
+  allocate(F(nf), TF(nf,nch-2,2), TFVar(nf,nch-2,2), TFName(nch-2,2), stat=istat)
+  allocate(InvSigCov(nf,2,2), ResidCov(nf,nch-2,nch-2), stat=istat)
+  allocate(U(2,2), V(nch-2,nch-2), stat=istat)
+
+  call read_z_channels(InputChannel, OutputChannel, zLocalSite%Declination)
+
+  ! Initialize conversion to orthogonal geographic coords
+  if (UserInfo%OrthogonalGeographic > 0) then
+     call rotate_z_channels(InputChannel, OutputChannel, U, V)
+  end if
+
+  do k=1,nf
+
+     !write (*,*) 'Reading period number ', k
+     call read_z_period(F(k), TF(k,:,:), TFVar(k,:,:), InvSigCov(k,:,:), ResidCov(k,:,:))
+
+     ! rotate to orthogonal geographic coordinates (generality limited to 4 or 5 channels)
+     if (UserInfo%OrthogonalGeographic > 0) then
+     	call rotate_z_period(U, V, TF(k,:,:), TFVar(k,:,:), InvSigCov(k,:,:), ResidCov(k,:,:))
+     end if
+
+  end do
+
+  call read_z_notes(Notes)
+
+  ! Finished reading Z-file
+
+  ! Read information for this site from a list. If successfully read,
+  ! trust this information rather than that from the Z-file
+  call read_site_list(UserInfo%SiteList, zLocalSite%ID, xmlLocalSite, site_list_exists)
+
+  if (len_trim(xmlLocalSite%ID)>0) then
+
+  	call add_xml_header(xmlLocalSite, UserInfo, Info, Notes)
+  else
+
+  	call add_xml_header(zLocalSite, UserInfo, Info, Notes)
+  end if
+
+  ! Read runs (e.g. start and end times) information for each of the runs
+  ! which were used in processing the data found in the Z-file
+  call read_run_list(UserInfo%RunList, zLocalSite%RunList, Run, run_list_exists)
+
+  ! Field notes go first
+  if (run_list_exists) then
+  	do i=1,size(Run)
+		call read_channel_list(UserInfo%ChannelList, Run(i)%ID, InputChannel, channel_list_exists)
+		call read_channel_list(UserInfo%ChannelList, Run(i)%ID, OutputChannel, channel_list_exists)
+		if (channel_list_exists) then
+			call add_FieldNotes(Run(i),InputChannel,OutputChannel)
+		else
+			call add_FieldNotes(Run(i))
+		end if
+  	end do
+  end if
+  
+  ! Processing notes follow
+  if (Info%remote_ref) then
+  	call read_site_list(UserInfo%SiteList, Info%remote_site_id, xmlRemoteSite, site_list_exists)
+	call read_run_list (UserInfo%RunList, xmlRemoteSite%RunList, RemoteRun, run_list_exists)
+  	if (run_list_exists) then
+		call add_ProcessingInfo(UserInfo, Info, xmlRemoteSite, RemoteRun)
+	else
+		call add_ProcessingInfo(UserInfo, Info, xmlRemoteSite)
+	end if
+  else
+  	call add_ProcessingInfo(UserInfo, Info)
+  end if
+
+  call new_channel_block('InputChannels')
+  do i=1,2
+     call add_Channel(InputChannel(i), location=.false.)
+  end do
+  call end_block('InputChannels')
+
+  call new_channel_block('OutputChannels')
+  do i=1,nch-2
+     call add_Channel(OutputChannel(i), location=.false.)
+  end do
+  call end_block('OutputChannels')
+
+  ! Read and write frequency blocks: transfer functions, variance, covariance
+  call initialize_xml_freq_block_output(nf)
+
+  do k=1,nf
+
+	 call new_data_block('Period',F(k))
+
+     call add_TF(TF(k,:,:), InputChannel, OutputChannel)
+     call add_TFVar(TFVar(k,:,:), InputChannel, OutputChannel)
+     call add_InvSigCov(InvSigCov(k,:,:), InputChannel)
+     call add_ResidCov(ResidCov(k,:,:), OutputChannel)
+
+     call end_block('Period')
+
+  end do
+
+  call end_xml_freq_block_output
+
+  !call add_PeriodRange(F)
+
+  ! Exit nicely
+  deallocate(Run)
+  if (associated(RemoteRun)) deallocate(RemoteRun)
+  if (associated(Notes)) deallocate(Notes)
+  deallocate(InputChannel, OutputChannel)
+  deallocate(F, TF, TFVar, InvSigCov, ResidCov)
+
+  call end_z_input
+
+  call end_xml_output('EM_TF')
+
+  if (.not.silent) then
+     write(*,*) 'Written to file: ',xml_file
+  end if
+
+end program edi2xml
---
+++ f90/edi_read.f90
@@ -0,0 +1,422 @@
+module edi_read
+
+  use global
+  use utils
+
+  implicit none
+  private
+
+  integer                        :: zfile
+  character(len=120)             :: temp
+  integer                        :: ios,i,j
+
+  save   :: zfile
+
+  public :: initialize_z_input
+  public :: read_z_header
+  public :: read_z_channels
+  public :: read_z_period
+  public :: rotate_z_channels
+  public :: rotate_z_period
+  public :: read_z_notes
+  public :: end_z_input
+
+contains
+
+  subroutine initialize_z_input(fname)
+     character(len=*), intent(in) :: fname
+     integer			  :: ios
+     character(20)		  :: str
+
+     ! passed an empty string
+     if (fname == '') then
+        zfile=5
+        open (unit=zfile,iostat=ios)
+     else
+        zfile=55
+        open (unit=zfile,file=fname,status='old',iostat=ios)
+     end if
+
+     if(ios/=0) then
+        write(0,*) 'Error opening file:', fname
+     endif
+
+  end subroutine initialize_z_input
+
+  !--------------------------------------------------------------------
+  ! Parses the station, as written into a Gary Egbert's Z-file, or the
+  ! name of a Z-file, such as: WAA05bc_A4
+  ! Assumes no extra characters at the end of this string, and uses
+  ! the following convention:
+  ! a) no underscore - assume single station
+  ! b) 2-char after the underscore - insert a zero, e.g. A4 -> WAA04
+  ! c) 3-char atter the underscore - assume 2 digits, e.g. A11 -> WAA11
+  ! d) <2-char or >3-char after the underscore - do not attempt to parse
+  ! Most importantly, assumes a 5-digit site name!!!!!!
+  ! Please manually correct the station name in your Z-file to satisfy
+  ! these criteria before attempting to run this program.
+  subroutine parse_z_site_name(sitename, SiteID, RemoteSiteID, RunList)
+  	character(len=80), intent(in)    :: sitename
+  	character(len=5), intent(out)    :: SiteID
+  	character(len=5), intent(out)    :: RemoteSiteID
+  	character(len=80), intent(out)   :: RunList
+  	character(len=80)                :: info
+	character(len=20)                :: list, abbrev
+  	integer                          :: l, k, i
+
+  	SiteID = toupper(sitename(1:1))//toupper(sitename(2:2))//toupper(sitename(3:3))//sitename(4:5)
+	RunList = ' '
+
+    ! Find spaces in the sitename
+    i = index(trim(sitename),' ')
+    if (i > 0) then
+        l = i-1
+    else
+  	    l = len_trim(sitename)
+  	end if
+
+  	if (l>5) then
+  		i = index(sitename,'_')
+		if (i==0) then ! Single Station - create the run list and exit
+			list = sitename(6:l)
+			abbrev = ' '
+		else ! Remote Reference - parse the name of the remote site
+			list = sitename(6:i-1)
+			abbrev = sitename(i+1:l)
+		end if
+		! In either case, make a list of run names, store them in a string
+		do k=1,len_trim(list)
+			RunList = trim(RunList)//' '//trim(SiteID)//list(k:k)
+		end do
+	end if
+
+  	if (len_trim(abbrev)==0) then
+  		RemoteSiteID = ' '
+  	else if (len_trim(abbrev)==2) then
+  		RemoteSiteID = SiteID(1:2)//toupper(abbrev(1:1))//'0'//abbrev(2:2)
+  	else if (len_trim(abbrev)>=3) then
+  		if (isdigit(abbrev(3:3))) then
+  			RemoteSiteID = SiteID(1:2)//toupper(abbrev(1:1))//abbrev(2:3)
+  		else ! Account for additional characters at the end e.g. ORG05bc_H6x
+   			RemoteSiteID = SiteID(1:2)//toupper(abbrev(1:1))//'0'//abbrev(2:2)
+  		end if
+	else
+		write(0,*) 'Unable to extract remote reference site from the string ',trim(sitename)
+		RemoteSiteID = ' '
+	end if
+
+    i = index(sitename,'remote')
+    l = len_trim(sitename)
+    if (i > 0) then
+        write(*,*) 'Using remote site ID from the Z-file header...'
+        RemoteSiteID = sitename(i+7:l)
+    end if
+
+  end subroutine parse_z_site_name
+
+
+  subroutine read_z_header(sitename, Site, Info)
+    character(len=80), intent(out)   :: sitename
+    type(Site_t),  intent(out)       :: Site
+	type(RemoteRef_t), intent(out)   :: Info
+
+	call init_remote_ref(Info)
+	call init_site_info(Site)
+
+    read (zfile,*) temp
+    read (zfile,*) temp
+    read (zfile,'(a80)') Info%remote_ref_type
+    read (zfile,'(a12,a80)') temp, sitename
+
+	call parse_z_site_name(sitename, Site%ID, Info%remote_site_id, Site%RunList)
+
+    if (.not.silent) then
+       write(*,*) trim(sitename),': local ',Site%ID,' remote ',Info%remote_site_id
+    end if
+
+	if (index(Info%remote_ref_type,'Remote Reference')>0) then
+		if (len_trim(Info%remote_site_id)>0) then
+			Info%remote_ref = .TRUE.
+		end if
+	end if
+
+	!Info%processed_by = processed_by
+	Info%processing_tag = sitename
+	!Info%software = 'EMTF'
+
+    read (zfile,'(a120)',iostat=ios) temp
+    i = index(temp,'coordinate')
+    j = index(temp,'declination')
+
+    read (temp(i+12:j-1),*) Site%Location%lat, Site%Location%lon
+    read (temp(j+12:120),*) Site%Declination
+    
+    if(Site%Location%lon > 180.0d0) then
+		Site%Location%lon = Site%Location%lon - 360.0d0
+	end if
+
+    read (zfile,'(a120)',iostat=ios) temp
+    i = index(temp,'channels')
+    j = index(temp,'frequencies')
+
+    read (temp(i+9:j-1),*) nch
+    read (temp(j+12:120),*) nf
+
+    if (.not.silent) then
+       write(*,*) 'Number of channels ', nch
+       write(*,*) 'Number of periods  ', nf
+    end if
+
+  end subroutine read_z_header
+
+
+  subroutine read_z_channels(Input, Output, decl)
+    type(Channel_t), dimension(:), intent(inout) :: Input
+    type(Channel_t), dimension(:), intent(inout) :: Output
+    real(8), intent(inout), optional             :: decl
+    real(8)                          :: declination
+    character(len=3)                 :: temp
+    integer                          :: num
+    character(len=80)                :: chname
+    real                             :: orientation
+    real                             :: tilt
+
+    read (zfile,*) temp !read the first comment
+
+    if (.not. present(decl)) then
+    	declination = 0.0
+    else
+    	declination = decl
+    end if
+
+    do i=1,2
+       read (zfile,*) num, orientation, tilt, temp, chname
+       call init_channel_info(Input(i))
+       Input(i)%ID = chname
+       Input(i)%orientation = orientation + declination
+       Input(i)%tilt = tilt
+       call init_channel_units(Input(i))
+    end do
+
+    do i=1,nch-2
+       read (zfile,*) num, orientation, tilt, temp, chname
+       call init_channel_info(Output(i))
+       Output(i)%ID = chname
+       Output(i)%orientation = orientation + declination
+       Output(i)%tilt = tilt
+       call init_channel_units(Output(i))
+    end do
+
+    read (zfile, *) ! read empty line at the end
+
+  end subroutine read_z_channels
+
+
+  subroutine read_z_period(F, TF, TFVar, InvSigCov, ResidCov)
+    type(FreqInfo_t),           intent(out)   :: F
+    complex(8), dimension(:,:), intent(inout) :: TF
+    real(8),    dimension(:,:), intent(inout) :: TFVar
+    complex(8), dimension(:,:), intent(inout) :: InvSigCov
+    complex(8), dimension(:,:), intent(inout) :: ResidCov
+    real(8)           :: period
+    integer           :: dec_level
+    integer           :: num_points
+    real              :: sampling_freq
+    character(len=10) :: units
+
+    read (zfile,'(a100)',iostat=ios) temp
+    i = index(temp,'period')
+    !j = index(temp,'decimation level')
+
+	call init_freq_info(F)
+
+    read (temp(i+8:i+24),*) period
+    !read (temp(i+8:j-1),*) period
+    !read (temp(j+16:100),'(i3)') dec_level
+    F%value = period   !1.0d0/period
+    F%units = 'secs'   ! Hz
+    F%info_type  = 'period' !'frequency'
+    !F%dec_level = dec_level
+
+    read (zfile,'(a100)',iostat=ios) temp
+    i = index(temp,'data point')
+    !j = index(temp,'sampling freq')
+
+    read (temp(i+10:i+18),*) num_points
+    !read (temp(i+10:j-1),'(i8)') num_points
+    !read (temp(j+14:100),*) sampling_freq, units
+    F%num_points = num_points
+
+    TF=0.0d0
+    read (zfile,*) temp !Transfer Functions
+    do i=1,nch-2
+       do j=1,2
+          read (zfile,'(2E12.3)',iostat=ios,advance='no') TF(i,j)
+       end do
+       read (zfile,*)
+    end do
+
+    InvSigCov=0.0d0
+    read (zfile,*) temp !Inverse Coherent Signal
+    do i=1,2
+       do j=1,i
+          read (zfile,'(2E12.3)',iostat=ios,advance='no') InvSigCov(i,j)
+          if (j<i) then
+             InvSigCov(j,i) = conjg(InvSigCov(i,j))
+          end if
+       end do
+       read (zfile,*)
+    end do
+
+    ResidCov=0.0d0
+    read (zfile,*) temp !Residual Covariance
+    do i=1,nch-2
+       do j=1,i
+          read (zfile,'(2E12.3)',iostat=ios,advance='no') ResidCov(i,j)
+          if (j<i) then
+             ResidCov(j,i) = conjg(ResidCov(i,j))
+          end if
+       end do
+       read (zfile,*,iostat=ios)
+    end do
+
+    TFVar=0.0d0
+    do i=1,nch-2
+       do j=1,2
+          TFVar(i,j) = (ResidCov(i,i)*InvSigCov(j,j))/2
+       end do
+    end do
+
+  end subroutine read_z_period
+
+
+  subroutine rotate_z_channels(Input, Output, U, V, azimuth)
+    type(Channel_t), dimension(:), intent(inout)   :: Input
+    type(Channel_t), dimension(:), intent(inout)   :: Output
+   	real(8),         dimension(:,:), intent(out)   :: U, V ! rotation matrices
+   	real(8),         optional, intent(inout)       :: azimuth ! new azimuth
+    ! local variables
+    real(8)                                        :: theta0
+    real(8),         dimension(:), allocatable     :: theta, thetanew
+    integer                                        :: nch, i, j, istat
+
+    nch = size(Input) + size(Output)
+
+	! note: U and V must be allocated before calling this subroutine
+    allocate(theta(nch), thetanew(nch), stat = istat)
+
+	if (.not. present(azimuth)) then
+    	theta0 = 0.0
+    else
+    	theta0 = azimuth
+	end if
+
+    ! assuming Hx and Hy are the input channels, - pretty much always the case
+    theta(1) = Input(1)%orientation; thetanew(1) = theta0
+    theta(2) = Input(2)%orientation; thetanew(2) = theta0 + 90.0
+
+    U(1,1) = cos(D2R*(theta(1) - theta0))
+    U(1,2) = sin(D2R*(theta(1) - theta0))
+    U(2,1) = cos(D2R*(theta(2) - theta0))
+    U(2,2) = sin(D2R*(theta(2) - theta0))
+
+	call inverse22(U,U);
+
+	! if there are 3 output channels, assume the first is Hz; otherwise no Hz
+	if (size(Output) == 3) then
+
+		theta(3) = Output(1)%orientation; thetanew(3) = 0.0
+		theta(4) = Output(2)%orientation; thetanew(4) = theta0
+		theta(5) = Output(3)%orientation; thetanew(5) = theta0 + 90.0
+
+		V(:,:) = 0.0
+		V(1,1) = 1.0
+		V(2,2) = cos(D2R*(theta(4) - theta0))
+		V(2,3) = cos(D2R*(theta(5) - theta0))
+		V(3,2) = sin(D2R*(theta(4) - theta0))
+		V(3,3) = sin(D2R*(theta(5) - theta0))
+
+	else if (size(Output) == 2) then
+
+		theta(3) = Output(1)%orientation; thetanew(3) = theta0
+		theta(4) = Output(2)%orientation; thetanew(4) = theta0 + 90.0
+
+    	V(1,1) = cos(D2R*(theta(3) - theta0))
+    	V(1,2) = cos(D2R*(theta(4) - theta0))
+    	V(2,1) = sin(D2R*(theta(3) - theta0))
+    	V(2,2) = sin(D2R*(theta(4) - theta0))
+
+	else
+		write(*,*) 'Unable to create rotation matrices: too many channels...'
+		deallocate(theta, thetanew)
+		call identity(U)
+		call identity(V)
+		return
+	end if
+
+	! define the new channel orientations
+    do i=1,2
+		Input(i)%orientation = thetanew(i)
+    end do
+
+    do i=1,nch-2
+		Output(i)%orientation = thetanew(i+2)
+    end do
+
+	deallocate(theta, thetanew)
+
+  end subroutine rotate_z_channels
+
+
+  subroutine rotate_z_period(U, V, TF, TFVar, InvSigCov, ResidCov)
+   	real(8),         dimension(:,:), intent(in)    :: U ! rotation matrix size 2x2 for input channels
+   	real(8),         dimension(:,:), intent(in)    :: V ! rotation matrix size (nch-2)x(nch-2) for output channels
+    complex(8),      dimension(:,:), intent(inout) :: TF
+    real(8),         dimension(:,:), intent(inout) :: TFVar
+    complex(8),      dimension(:,:), intent(inout) :: InvSigCov
+    complex(8),      dimension(:,:), intent(inout) :: ResidCov
+    ! local variables
+    integer                                        :: i, j
+
+	! rotate the transfer functions and covariance matrices (using * gives strange results!)
+	TF = matmul(V, matmul(TF,transpose(U)))
+	InvSigCov = matmul(U, matmul(InvSigCov,transpose(U)))
+	ResidCov = matmul(V, matmul(ResidCov,transpose(V)))
+
+	! finally, update the variances
+    do i=1,size(V,1)
+       do j=1,2
+          TFVar(i,j) = (ResidCov(i,i)*InvSigCov(j,j))/2
+       end do
+    end do
+
+  end subroutine rotate_z_period
+
+
+  subroutine read_z_notes(Notes)
+  	character(100), dimension(:), pointer :: Notes
+	integer                               :: n
+
+	read (zfile,'(i8)',iostat=ios) n
+	if (ios/=0) then
+		if (associated(Notes)) deallocate(Notes)
+		return
+	end if
+
+	if (associated(Notes)) deallocate(Notes)
+
+	allocate(Notes(n))
+
+	do i=1,n
+		read (zfile,'(a100)',iostat=ios) Notes(i)
+	end do
+
+  end subroutine read_z_notes
+
+
+  subroutine end_z_input
+
+    close(zfile)
+  end subroutine end_z_input
+
+end module edi_read
